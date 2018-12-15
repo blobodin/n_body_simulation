c2f7981b80df74f8959c3dfe385d8c060f6e1a4c
Adam Blank <blank@sneetch.local>
2018-12-15 08:09:48 -0800

    Initial commit

---
+++ src/edu/caltech/cs2/project01/Body.java
@@ -0,0 +1,27 @@
+package edu.caltech.cs2.project01;
+
+import edu.caltech.cs2.libraries.IBody;
+import edu.caltech.cs2.libraries.Vector2D;
+
+public class Body implements IBody<Body> {
+
+  @Override
+  public void calculateNewForceFrom(Body[] bodies) {
+
+  }
+
+  @Override
+  public void updatePosition(double dt) {
+
+  }
+
+  @Override
+  public Vector2D getCurrentPosition() {
+    return null;
+  }
+
+  @Override
+  public String getFileName() {
+    return null;
+  }
+}
---
+++ src/edu/caltech/cs2/project01/NBodySimulation.java
@@ -0,0 +1,5 @@
+package edu.caltech.cs2.project01;
+
+public class NBodySimulation {
+
+}
