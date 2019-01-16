8c0ecf9c4e2995571e19a69be86aa544e614fe40
blobodin <blobodin@caltech.edu>
2019-01-16 10:52:37 -0800

    Initial commit

--- src/edu/caltech/cs2/project01/Body.java
+++ src/edu/caltech/cs2/project01/Body.java
@@ -3,25 +3,93 @@ package edu.caltech.cs2.project01;
 import edu.caltech.cs2.libraries.IBody;
 import edu.caltech.cs2.libraries.Vector2D;
 
+import static java.lang.Math.*;
+
 public class Body implements IBody<Body> {
+  private final double mass;
+  private final String filename;
+  private Vector2D position;
+  private Vector2D velocity;
+  private Vector2D force;
+  private static final double G = (.0000000000667);
+
+  public Body(Vector2D position, Vector2D velocity, double mass, String filename) {
+    this.position = position;
+    this.velocity = velocity;
+    this.mass = mass;
+    this.filename = filename;
+    this.force = new Vector2D(0, 0);
+
+
+  }
+
+  public Body(double xPos, double yPos, double xVel, double yVel, double mass, String filename) {
+    this(new Vector2D(xPos, yPos), new Vector2D(xVel, yVel), mass, filename);
+
+  }
+
+  public Body(double xPos, double yPos, double xVel, double yVel) {
+    this(new Vector2D(xPos, yPos), new Vector2D(xVel, yVel), 0.0, null);
+  }
+
+  private Vector2D getForceFrom(Body other) {
+    double r = distanceTo(other);
+    double F = (G * this.mass * other.mass) / (pow(r, 2));
+    double cos = (other.position.getX() - this.position.getX()) / r;
+    double sin = (other.position.getY() - this.position.getY()) / r;
+    double x = (F * cos);
+    double y = (F * sin);
+    Vector2D force = new Vector2D(x, y);
+    return force;
+
+  }
 
   @Override
   public void calculateNewForceFrom(Body[] bodies) {
+    double net_force_x = 0.0;
+    double net_force_y = 0.0;
+    Vector2D net = new Vector2D(0, 0);
+    for (Body b : bodies) {
+      if (b != this) {
+        net = this.getForceFrom(b);
+        net_force_x += net.getX();
+        net_force_y += net.getY();
+      }
+    }
+    this.force = new Vector2D(net_force_x, net_force_y);
 
   }
 
   @Override
   public void updatePosition(double dt) {
-
+    double xAcc = this.force.getX() / this.mass;
+    double yAcc = this.force.getY() / this.mass;
+    this.velocity = new Vector2D(Double.valueOf(this.velocity.getX()) + Double.valueOf((dt * xAcc)),
+            Double.valueOf(this.velocity.getY()) + Double.valueOf((dt * yAcc)));
+    this.position = new Vector2D(Double.valueOf(this.position.getX()) + Double.valueOf(dt * this.velocity.getX()),
+            Double.valueOf(this.position.getY()) + Double.valueOf(dt * this.velocity.getY()));
   }
 
   @Override
   public Vector2D getCurrentPosition() {
-    return null;
+
+    return this.position;
   }
 
   @Override
   public String getFileName() {
-    return null;
+    return this.filename;
+  }
+
+  private double distanceTo(Body other) {
+    double distance = sqrt(pow((this.position.getX() - other.position.getX()), 2)
+            + pow((this.position.getY() - other.position.getY()), 2));
+    return distance;
+  }
+
+  public String toString() {
+    Vector2D position = this.getCurrentPosition();
+    return String.format("%11.4e %11.4e %11.4e %11.4e %11.4e %12s ", position.getX(), position.getY(),
+            this.velocity.getX(), this.velocity.getY(), this.mass, this.getFileName()).stripTrailing();
   }
 }
--- src/edu/caltech/cs2/project01/NBodySimulation.java
+++ src/edu/caltech/cs2/project01/NBodySimulation.java
@@ -1,5 +1,117 @@
 package edu.caltech.cs2.project01;
 
+import edu.caltech.cs2.libraries.IBody;
+import edu.caltech.cs2.libraries.Vector2D;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+import static edu.caltech.cs2.libraries.StdDraw.*;
+
 public class NBodySimulation {
 
+    public static void main(String[] args) throws FileNotFoundException {
+        double T = Double.parseDouble(args[0]);
+        double dt = Double.parseDouble(args[1]);
+        String file= args[2];
+        Scanner in = new Scanner(new File(file));
+        int numBodies = 0;
+        int j = 0;
+        double radius = 0;
+        while(in.hasNextLine()){
+            if (j == 0){
+                numBodies = Integer.parseInt(in.nextLine());
+            }
+            if (j == 1){
+                radius = Double.parseDouble(in.nextLine());
+            }
+            j++;
+            if(j > 2){
+                break;
+            }
+        }
+
+        Body[] bodies = readBodies(in, numBodies);
+        setupDrawing(radius);
+        for(double t = 0.0; t < T; t += dt){
+            drawStep(bodies);
+            calculateStep(bodies, dt);
+        }
+        printState(radius, bodies);
+    }
+
+    public static Body[] readBodies(Scanner in, int numBodies) {
+        Body[] bodies = new Body[numBodies];
+        if (numBodies == 0){
+            return bodies;
+        }
+        int i = 0;
+        while(in.hasNextLine()) {
+            int j = 0;
+            String line = "";
+            while(in.hasNext()){
+                line += in.next() + " ";
+                if(j == 5){
+                    break;
+                }
+                j++;
+            }
+            String[] parameters = line.trim().split(" ");
+            if(parameters.length == 6){
+                Body body = new Body(Double.parseDouble(parameters[0]),
+                        Double.parseDouble(parameters[1]),
+                        Double.parseDouble(parameters[2]),
+                        Double.parseDouble(parameters[3]),
+                        Double.parseDouble(parameters[4]),
+                        parameters[5]);
+                bodies[i] = body;
+
+            }
+            i++;
+            if(i == numBodies) {
+                break;
+            }
+        }
+        return bodies;
+    }
+
+    public static void calculateStep(Body[] bodies, double dt) {
+        for(int i = 0; i < bodies.length; i ++){
+            bodies[i].calculateNewForceFrom(bodies);
+        }
+        for(int i = 0; i < bodies.length; i++) {
+            bodies[i].updatePosition(dt);
+        }
+    }
+    public static void setupDrawing(double radius) {
+        enableDoubleBuffering();
+        setScale(-radius, radius);
+    }
+
+    public static void drawStep(IBody[] bodies) {
+        picture(0, 0, "data/images/starfield.jpg");
+        for (int i = 0; i < bodies.length; i++) {
+            double[] coordinates = new double[2];
+            Vector2D vector = bodies[i].getCurrentPosition();
+            coordinates[0] = vector.getX();
+            coordinates[1] = vector.getY();
+            String filename = "data/images/" + bodies[i].getFileName();
+            picture(coordinates[0], coordinates[1], filename);
+        }
+        show();
+    }
+
+    public static void printState(double radius, IBody[] bodies){
+        System.out.printf("%d" + "\n" + "%.2e".strip() + "\n", bodies.length, radius);
+        for(int i = 0; i < bodies.length; i++){
+            if(i == bodies.length-1){
+                System.out.print(bodies[i].toString());
+            }
+            else {
+                System.out.print(bodies[i].toString() + "\n");
+            }
+        }
+    }
 }
